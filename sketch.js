f54f67bbc15d97377e2a9efa71ee7744f441aad2
BazziBeatrice <55453353+BazziBeatrice@users.noreply.github.com>
2019-10-01 23:28:45 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,36 @@
-function preload(){
-  // put preload code here
-}
+let xoff = 0.0;
+let xincrement = 0.1;
+let x = 0;
+let y = 0;
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  noStroke();
 }
 
 function draw() {
-  // put drawing code here
-}
+  background(0);
+
+
+  let c = map(mouseX, 0, width, 0, 100);
+
+  let d = map(mouseX, 0, width, 50, 350);
+
+  for (var a = 0; a < windowWidth; a+=100){
+      for (var b = 0; b < windowHeight; b+=100){
+        fill(0, c, a);
+        ellipse(a, b, d, d);
+      }
+  }
+
+  x = lerp(x, mouseX, 0.1);
+  y = lerp(y, mouseY, 0.1);
+
+  fill(255);
+  stroke(255);
+  ellipse(x, y, 50, 50);
+
+  textSize(16);
+  textAlign(CENTER);
+  text('Follow your dot', x + 100, y - 50);
+  }
